d3b7e13db1d605357c3f0c4efcc8b6b49bbf18df
Vasanthan <evasanthan@gmail.com>
2018-03-31 16:59:44 +0530

    commit;

---
+++ ci/tasks/package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e +x
+
+pushd VSM-Stateful-POC-source
+  echo "Packaging Zip"
+  	zip -r source.zip .
+popd
+
+echo "Done zipping"
+
+# jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
+# 
+# if [ $jar_count -gt 1 ]; then
+#   echo "More than one jar found, don't know which one to deploy. Exiting"
+#   exit 1
+# fi
+# 
+# find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
+
+
+exit 0
