eea7fdd2006b2745d7a0dae169df78d84643b441
Vasanthan <evasanthan@gmail.com>
2018-04-04 15:22:19 +0530

    pipeline image resources have modified

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,7 +2,7 @@
 
 set -e +x
 
-pushd Stateful-source
+pushd Stateful
   echo "Packaging Zip"
   	zip -r source.zip .
 popd
@@ -10,12 +10,12 @@ popd
 echo "Done zipping"
 
 # jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
-# 
+#
 # if [ $jar_count -gt 1 ]; then
 #   echo "More than one jar found, don't know which one to deploy. Exiting"
 #   exit 1
 # fi
-# 
+#
 # find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
 
 
